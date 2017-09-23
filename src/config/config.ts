32457c3dfeeaca0577632c7775e38ced96b97e19
gibsonxiong <442760763@qq.com>
2017-09-23 19:51:03 +0800

    移动config.ts到config/config.ts

---
+++ src/config/config.ts
@@ -0,0 +1,7 @@
+//服务器地址
+export const HOST = 'http://192.168.1.38';
+// export const HOST = 'http://192.168.0.105';
+//api根地址
+export const API_HOST = HOST + '/api';
+//上传文件地址
+export const UPLOAD_HOST = HOST + '';
