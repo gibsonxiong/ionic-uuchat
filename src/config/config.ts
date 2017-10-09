38f2cf7ca12f25c0e0d87526123d00bf1e816e3f
gibsonxiong <442760763@qq.com>
2017-10-09 11:30:15 +0800

    更改配置

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 //服务器地址
-export const HOST = 'http://192.168.0.110:3000';
-// export const HOST = '';
+// export const HOST = 'http://192.168.0.110:3000';
+export const HOST = '';
 //api根地址
 export const API_HOST = HOST + '/api';
 //上传文件地址
